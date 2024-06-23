2dac6c67de4ce45194bd79647a9a4e2bb1613ba4
svein-salazar <sal22020@byui.edu>
2024-06-22 22:59:44 -0600

    draft of activity of temples galery

---
+++ scripts/temples.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const currentYear = new Date().getFullYear();
+    document.getElementById('currentyear').textContent = currentYear;
+
+    const lastModified = document.lastModified;
+    document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
+
+    const hamburger = document.getElementById('hamburger');
+    const navMenu = document.querySelector('nav ul');
+
+    hamburger.addEventListener('click', () => {
+        navMenu.classList.toggle('open');
+        hamburger.textContent = navMenu.classList.contains('open');
+    });
+});
